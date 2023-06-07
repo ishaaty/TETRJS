d08a7d4b2234094bbe1a3d94d842eaefb9068cd5
Julia Thompson <jewelst2014@gmail.com>
2023-06-07 08:19:05 -0400

    Creates new blocks!!

--- eventHandlers.js
+++ eventHandlers.js
@@ -4,8 +4,8 @@ let clearBtn = document.querySelector("#clear");
 let color = "black";
 let gridList = [];
 
-for(let x = 0; x <= canvas.height; x += 40){
-    for (let y = 0; y <= canvas.width; y += 40){
+for (let x = 0; x <= canvas.height; x += 40) {
+    for (let y = 0; y <= canvas.width; y += 40) {
         let pixel = new Pixel(x, y);
         gridList.push(pixel);
     }
@@ -13,6 +13,9 @@ for(let x = 0; x <= canvas.height; x += 40){
 
 // movement based on input
 window.addEventListener("keydown", function (e) {
+    if (e.key == "ArrowDown") {
+        block.slowDrop();
+    }
     if (e.repeat) {
         return;
     }
@@ -21,9 +24,6 @@ window.addEventListener("keydown", function (e) {
     }
     if (e.key == "ArrowRight") {
         block.moveRight();
-    }
-    if (e.key == "ArrowDown") {
-        block.slowDrop();
     }
     if (e.key == " ") {
         block.quickDrop();
@@ -36,17 +36,21 @@ window.addEventListener("load", function () {
     // creating the grid
     let pixelGrid = gridList[0]; // contains coordinates (0,0)
     pixelGrid.makeOrColorGrid(0, 0, canvas.width, canvas.height, 40);
-
     createBlock(pixelGrid);
-    
-      
+    // not 100% but progress
+    v = setInterval(function () {
+        if (!(block.pixelList[0].yCoordinate + 40 <= canvas.height)) {
+            console.log("made")
+            createBlock(pixelGrid);
+        }
+    }, 1000);
 });
 
 
 function createBlock(pixelGrid) {
     let v;
     let rand = Math.floor(Math.random() * 7);
-    switch(rand) {
+    switch (rand) {
         case 0:
             block = new Square(160, 40, pixelGrid);
             break;
@@ -68,21 +72,20 @@ function createBlock(pixelGrid) {
         case 6:
             block = new Line(160, 40, pixelGrid);
     }
-    v = setInterval(function() {
+    v = setInterval(function () {
         block.moveDown();
         // if (block.pixelList[block.pixelList.length -1].yCoordinate + 40 == canvas.height){
-        for (let i = 0; i < block.pixelList.length; i++){
+        for (let i = 0; i < block.pixelList.length; i++) {
             // also add if block underneath is not available (use isAvailable and changeAvailability)
-            if (block.pixelList[i].yCoordinate >= canvas.height){
+            if (block.pixelList[i].yCoordinate >= canvas.height) {
                 clearInterval(v);
-                for (let i = 0; i < block.pixelList.length; i++){
+                for (let i = 0; i < block.pixelList.length; i++) {
                     console.log(block.pixelList[i].isAvailable);
                     block.pixelList[i].changeAvailability();
                     console.log(block.pixelList[i].isAvailable);
                 }
             }
         }
-            
     }, 1000);
 }
 
