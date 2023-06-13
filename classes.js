2fc581cb05a6a0229d8a024ee07793227599d6e1
JohnJosephLanda <jjl2008.com@gmail.com>
2023-06-13 11:38:17 -0400

    fixed quickDrop and slowDrop

--- classes.js
+++ classes.js
@@ -107,15 +107,43 @@ class Block {
     }
 
     slowDrop(){
-        if (block.pixelList[0].yCoordinate + 40 < canvas.height && block.pixelList[1].yCoordinate + 40 < canvas.height && block.pixelList[2].yCoordinate + 40 < canvas.height && block.pixelList[3].yCoordinate + 40 < canvas.height) {
-            this.moveDown();
-        } 
+        for (let i = 0; i < block.pixelList.length; i++) {
+            let nextY = block.pixelList[i].yCoordinate + 40;
+            if ((nextY == 800) || (gridList[nextY/40+1][block.pixelList[i].xCoordinate/40].isAvailable == false)) return;
+        }
+        this.moveDown();
+        
     }
 
     quickDrop(){
-        while (block.pixelList[0].yCoordinate + 40 < canvas.height && block.pixelList[1].yCoordinate + 40 < canvas.height && block.pixelList[2].yCoordinate + 40 < canvas.height && block.pixelList[3].yCoordinate + 40 < canvas.height) {
-            this.moveDown();
+        let dropping = true;
+        let timesDrop = 0;
+
+        let nextY, nextY1, nextY2, nextY3, nexts;
+        nextY = block.pixelList[0].yCoordinate;
+        nextY1 = block.pixelList[1].yCoordinate;
+        nextY2 = block.pixelList[2].yCoordinate;
+        nextY3 = block.pixelList[3].yCoordinate;
+
+        nexts = [nextY,nextY1,nextY2,nextY3];
+
+        while (dropping) {
+
+            for (let i = 0; i < nexts.length; i++) {
+                if ((nexts[i] == 800) || (gridList[nexts[i]/40+1][block.pixelList[i].xCoordinate/40].isAvailable == false)) {
+                    dropping = false;
+                }
+            }
+
+            timesDrop ++;
+
+            for (let i = 0; i < nexts.length; i++) {
+                nexts[i] += 40;
+            }
         } 
+        for (let i = 1; i < timesDrop-1; i++) {
+            this.moveDown();
+        }
     }
 
     rotate(){
